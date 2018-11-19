3acf7eb63272837161212881f06bba8ed402354c
David Bogaerts <dbogaerts@davids-mbp.lan>
2018-11-18 18:32:45 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,18 +2,24 @@ var cards = ['queen', 'queen', 'king', 'king'];
 
 let cardsInPlay = [];
 
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log('User flipped ' + cardOne);
-
-let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log('User flipped ' + cardTwo);
-
-if (cardsInPlay.length === 2) {
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-        alert('You found a match!');
-    } else {
-        alert('Sorry, try again.');
+const checkForMatch = function() {
+    if (cardsInPlay.length === 2) {
+        if (cardsInPlay[0] === cardsInPlay[1]) {
+            // console.log('You found a match!');
+            alert('You found a match!');
+        } else {
+            // console.log('Sorry, try again.');
+            alert('Sorry, try again.');
+        }
     }
 }
+
+function flipCard(cardId) {
+    console.log('User flipped ' + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+    checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
+
